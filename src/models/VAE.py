ccb9c537f9b6ee0cbcebed824b603f07fcc4f58b
Nakul Sharma <nakul120.ns@gmail.com>
2021-06-11 13:22:19 +0530

    fixed some errors in VAE module

--- src/models/VAE.py
+++ src/models/VAE.py
@@ -18,25 +18,25 @@ class VAE(pl.LightningModule):
         super(VAE, self).__init__()
 
         C, H, W = input_shape
-        self.encoder = encoder if encoder is None else EncoderBlock(C)
+        self.encoder = EncoderBlock(C) if encoder is None else encoder
         self.latent_dim = latent_dim
 
-        enc_out_shape = self.decoder.get_output_shape(C, H, W)
+        enc_out_shape = self.encoder.get_output_shape(C, H, W)
 
         self.aux_channels = enc_out_shape[0]
         # the no. of output channels by encoder,
         # these will also be the number of channels we will be feed to decoder
 
-        self.decoder = decoder if decoder is None else\
-            DecoderBlock(self.aux_channels)
+        self.decoder = DecoderBlock(self.aux_channels, C) if decoder is None\
+            else decoder
 
-        # if output is CxHxW, we'll average out along H and W
+        # if output is CxHxW from Encoder, we'll average out along H and W
 
         self.mu_layer = nn.Linear(enc_out_shape[0], latent_dim)
         self.logvar_layer = nn.Linear(enc_out_shape[0], latent_dim)
 
-        self.decode_latent = nn.Linear(latent_dim, enc_out_shape[0]*4*4)
-        # this will be reshaped to Cx4x4
+        self.decode_latent = nn.Linear(latent_dim, self.aux_channels*4*4)
+        # this will be reshaped to self.aux_channelsx4x4
 
     def forward(self, x):
         x, mu, logvar = self.encode(x)
@@ -53,6 +53,9 @@ class VAE(pl.LightningModule):
 
     def encode(self, x):
         x = self.encoder(x)
+
+        # taking mean along H and W, similar to Global Average Pooling
+        x = x.mean([2, 3])
         mu = self.mu_layer(x)
         logvar = self.logvar_layer(x)
 
