cca7d4818cb86e17b84889ef6dd44ef23261863e
Nakul Sharma <nakul120.ns@gmail.com>
2021-06-11 13:15:51 +0530

    updated Decoder template

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 
 class DecoderBlock(nn.Module):
 
-    def __init__(self, latent_dim=256, out_channels=3):
+    def __init__(self, in_channels, out_channels=3):
 
-        self(DecoderBlock, self).__init__()
+        super(DecoderBlock, self).__init__()
 
     def forward(self, x):
         pass
